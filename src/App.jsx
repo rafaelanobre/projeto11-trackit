b04df2294b97f33e909fe784a5247dd4b461831a
rafaelanobre <raafanobre@gmail.com>
2023-05-30 08:31:39 -0300

    :construction: Starting login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import Login from './pages/Login'
 
 function App() {
 
   return (
     <>
-      <h1>TrackIt</h1>
+      <Login />
     </>
   )
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,37 @@
 import React from "react"
+import logo from "../assets/Group8.png"
+import styled from "styled-components"
 
 export default function Login(){
     return(
         <>
-        <Logo></Logo>
-        <FormsDiv></FormsDiv>
-        <Link></Link>
+        <Logo src={logo}></Logo>
+        <FormsDiv>
+            <input placeholder="email" />
+            <input placeholder="senha" />
+            <button type="submit">Entrar</button>
+        </FormsDiv>
+        <Link>
+            <p>Não tem uma conta? Cadastre-se!</p>
+        </Link>
         </>
     )
 }
+
+const Logo = styled.img`
+    width: 180px;
+`;
+const FormsDiv = styled.form`
+    display: flex;
+    flex-direction: column;
+`;
+const Link = styled.div`
+    p{
+        font-weight: 400;
+        font-size: 14px;
+        text-align: center;
+        text-decoration-line: underline;
+
+        color: #52B6FF;
+    }
+`;
